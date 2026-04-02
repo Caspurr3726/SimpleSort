91f71d204c52562aaa1e943e0518c2c2b7228cdb
C4tpurr <caspercraft1205@gmail.com>
2026-04-02 12:44:39 -0400

    Commit before testing file moving

--- sort.py
+++ sort.py
@@ -1,35 +1,29 @@
 from pathlib import Path as p
 import os
+import shutil
 
-
-# Path of folder that needs to be sorted
+# Path of folder that needs to be sorted.
 unsorted = p("./unsorted")
-
 
 
 def main():
     sort()
     
 
-
 def sort():
+    # Creates a list, named files, containing every item that isn't a directory in the unsorted folder.
     files = [item for item in p.iterdir(unsorted) if not item.is_dir()]
+    # Runs through each file in files, checking their extension, and making a folder for that extension if one doesn't already exist.
     for file in files:
         _, extension = str(file).split(".")
         if p(extension).exists():
             pass
         else:
             os.mkdir(extension)
-            # print(f"Folder {extension} created at {os.getcwd()}")
+            # Debug : print(f"Folder {extension} created at {os.getcwd()}")
+        
+        shutil.move(file, p(extension))
     
-    #os.chdir(sorted)
-    #for extension in extensions:
-        #if p(extension).exists():
-            #pass
-        #else:
-            #os.mkdir(extension)
-            # print(f"Folder {extension} created at {os.getcwd()}")
-
 
 if __name__ == "__main__":
     main()
