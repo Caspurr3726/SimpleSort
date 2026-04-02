01fb9912acfc7826483e570ccf4b58850634fa85
C4tpurr <caspercraft1205@gmail.com>
2026-04-02 18:15:09 -0400

    Exception fix

--- sort.py
+++ sort.py
@@ -7,7 +7,7 @@ import sys
 
 parser = argparse.ArgumentParser(usage="Sorts files into folders based on file extensions", description="A simple file sorter by Caspurr3726")
 parser.add_argument("-cd", metavar= "Change Directory", default = "./unsorted", help = "change directory to pull files from", type = str)
-parser.add_argument("-e", metavar= "Exclude", default = [], help = "file extensions excluded from sorting", type = list)
+parser.add_argument("-e", metavar= "Exclude", default = [], help = "file extensions excluded from sorting", type = str, nargs="*")
 args = parser.parse_args()
 
 
@@ -21,13 +21,11 @@ def main():
 def sort():
     input_folder = p(args.cd)
     exceptions = args.e
-
-
     # Creates a list, named files, containing every item that isn't a directory in the unsorted folder.
     files = [item for item in p.iterdir(input_folder) if not item.is_dir()]
     # Runs through each file in files, checking their extension, and making a folder for that extension if one doesn't already exist.
     for file in files:
-        _, extension = str(file).split(".")
+        *other, extension,  = str(file).split(".")
         if extension in exceptions:
             continue
         if p(extension).exists():
@@ -38,6 +36,7 @@ def sort():
         
         
         shutil.move(file, p(extension))
+        print(f"Sorted all files except: {exceptions}")
 
 
 
